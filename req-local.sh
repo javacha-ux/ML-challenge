bb044740e1b57f61d0113980883be37899fc89e8
javacha-ux <javacha.ux@gmail.com>
2021-08-03 00:05:40 -0300

    se agrega script para correr prueba en local

---
+++ req-local.sh
@@ -0,0 +1,21 @@
+if [ -n "$1" ]
+then
+	payload=$1
+else
+	echo ""
+	echo "uso: "
+	echo ""
+	echo "$0   '{ \"dna\": [ \"CGC\", \"GTA\" ] }' "
+	echo ""
+	exit 1
+fi
+
+
+RET=$(curl -X   POST http://localhost:8090/mutant    -H 'Content-Type: application/json'    -d "${payload}")
+
+echo ""
+echo $RET
+echo ""
+
+
+
